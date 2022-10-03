ebf55b50d2bfabf0645fd470960e5ed8b87528e1
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-10-03 07:48:53 -0300

    update

--- src/main.c
+++ src/main.c
@@ -6,19 +6,18 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 17:52:02 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/10/02 21:35:45 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/10/03 03:04:20 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static int		pipex(t_args *data);
-static void		cmd_in(char *file, int *fd, t_args *data);
-static void		cmd_out(char *file, int *fd, t_args *data);
+static void		pipex(t_args *data);
+static void		cmd_in(int *fd, t_args *data);
+static void		cmd_out(int *fd, t_args *data);
 
 int	main(int argc, char **argv, char **envp)
 {
-	int		status;
 	t_args	data;
 
 	if (argc != 5)
@@ -27,64 +26,56 @@ int	main(int argc, char **argv, char **envp)
 		return (EXIT_FAILURE);
 	}
 	eval(argc, argv, envp, &data);
-	status = pipex(&data);
-	cleanup(data.cmd);
-	return (status);
-}
-
-static int	pipex(t_args *data)
-{
-	int	fd[2];
-	int	pid1;
-	int	pid2;
-	int	status;
-
-	if (pipe(fd) == -1)
-		return (EXIT_FAILURE);
-	pid1 = pfork();
-	if (pid1 == 0)
-		cmd_in(data->argv[1], fd, data);
-	pid2 = pfork();
-	if (pid2 == 0)
-		cmd_out(data->argv[4], fd, data);
-	close(fd[WRITE_END]);
-	close(fd[READ_END]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, &status, 0);
-	if (WIFEXITED(status))
-		return (WEXITSTATUS(status));
+	pipex(&data);
 	return (EXIT_SUCCESS);
 }
 
-static void	cmd_in(char *file, int *fd, t_args *data)
+static void	pipex(t_args *data)
 {
-	fd[READ_END] = open(file, O_RDONLY);
-	if (fd[READ_END] == -1)
+	int	fd[2];
+	int	pid;
+
+	if (pipe(fd) == -1)
+		err_exit("pipe");
+	pid = pfork();
+	if (pid == 0)
+		cmd_in(fd, data);
+	wait(NULL);
+	cmd_out(fd, data);
+}
+
+static void	cmd_in(int *fd, t_args *data)
+{
+	int		file_in;
+	char	*filename;
+
+	filename = data->argv[1];
+	file_in = open(filename, O_RDONLY);
+	if (file_in == -1)
 	{
 		cleanup(data->cmd);
-		err_exit(file);
+		err_exit(filename);
 	}
-	dup2(fd[READ_END], STDIN_FILENO);
+	dup2(file_in, STDIN_FILENO);
 	dup2(fd[WRITE_END], STDOUT_FILENO);
 	close(fd[READ_END]);
-	close(fd[WRITE_END]);
 	execp(data->cmd[0], data);
 }
 
-static void	cmd_out(char *file, int *fd, t_args *data)
+static void	cmd_out(int *fd, t_args *data)
 {
-	int	file_out;
+	int		file_out;
+	char	*filename;
 
-	file_out = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	filename = data->argv[4];
+	file_out = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (file_out == -1)
 	{
 		cleanup(data->cmd);
-		err_exit(file);
+		err_exit(filename);
 	}
 	dup2(fd[READ_END], STDIN_FILENO);
 	dup2(file_out, STDOUT_FILENO);
-	close(fd[READ_END]);
 	close(fd[WRITE_END]);
-	close(file_out);
 	execp(data->cmd[1], data);
 }
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 14:54:34 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/10/02 18:16:12 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/10/03 02:35:04 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	pfork(void)
 
 	pid = fork();
 	if (pid == -1)
-		perror("fork");
+		err_exit("fork");
 	return (pid);
 }
 
@@ -34,5 +34,6 @@ void	execp(char **cmd, t_args *data)
 	ft_putstr_fd("pipex: ", STDERR_FILENO);
 	ft_putstr_fd(cmd[0], STDERR_FILENO);
 	ft_putendl_fd(": command not found...", STDERR_FILENO);
+	cleanup(data->cmd);
 	exit(127);
 }
